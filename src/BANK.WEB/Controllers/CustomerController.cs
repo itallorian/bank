0d1829d4f9181029e2fed64c3555b2022b1241be
itallorian <italloriancs@gmail.com>
2022-04-22 08:05:00 -0300

    Page for customer registration

---
+++ src/BANK.WEB/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using BANK.WEB.ViewModels.Customer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BANK.WEB.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        [Route("~/register")]
+        public IActionResult Register([FromQuery(Name = "e")] string email)
+        {
+            RegisterViewModel viewModel = new RegisterViewModel()
+            {
+                Email = email
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/register")]
+        public IActionResult Register([FromForm] RegisterViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
---
+++ src/BANK.WEB/ViewModels/Customer/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace BANK.WEB.ViewModels.Customer
+{
+    public class RegisterViewModel
+    {
+        public string? Email { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? ConfirmPassword { get; set; }
+
+        #region [ Assistant ]
+
+        public bool ReadOnlyEmailInput
+        {
+            get
+            {
+                return string.IsNullOrEmpty(Email) == false;
+            }
+        }
+
+        #endregion [ Assistant ]
+    }
+}
