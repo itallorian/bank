073d4ee5f66dd4722baa8f2083d228adfb989afa
itallorian <italloriancs@gmail.com>
2022-04-23 16:29:03 -0300

    Users (Employees): Login page;

---
+++ src/BANK.WEB/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using BANK.WEB.ViewModels.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BANK.WEB.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet, Route("~/user")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("~/user")]
+        public IActionResult Login([FromForm] UserLoginViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
