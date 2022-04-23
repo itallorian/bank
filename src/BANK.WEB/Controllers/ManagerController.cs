ad0c6a72cf482cc66aa8dd88e4affae59b540cb0
itallorian <italloriancs@gmail.com>
2022-04-23 15:46:14 -0300

    Administrator Login page

---
+++ src/BANK.WEB/Controllers/ManagerController.cs
@@ -0,0 +1,20 @@
+﻿using BANK.WEB.ViewModels.Manager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BANK.WEB.Controllers
+{
+    public class ManagerController : Controller
+    {
+        [HttpGet, Route("~/management")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("~/management")]
+        public IActionResult Login([FromForm] ManagerLoginViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
