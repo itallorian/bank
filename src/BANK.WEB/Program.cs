c643adc4eeec990c92b9adebb4c56e328eab8021
itallorian <italloriancs@gmail.com>
2022-05-06 06:51:07 -0300

    hotfix repositories set

--- src/BANK.WEB/Program.cs
+++ src/BANK.WEB/Program.cs
@@ -6,6 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+#region [ SET REPOSITORIES ]
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IManagerRepository, ManagerRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+#endregion [ SET REPOSITORIES ]
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,14 +24,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-#region [ SET REPOSITORIES ]
-
-builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddScoped<IManagerRepository, ManagerRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-#endregion [ SET REPOSITORIES ]
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
